a7192a0b515073bed14ca416082c53c57e5c5b5f
Kevin Huang <67510498+KevinH0@users.noreply.github.com>
2023-09-28 19:54:04 -0400

    Movements
    Added basic vertical movement based on gravity and acceleration/drag into Kevin/Scene.

--- City Tech Fall 2023/Assets/Script/PlayerMovement.cs
+++ City Tech Fall 2023/Assets/Script/PlayerMovement.cs
@@ -6,10 +6,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed;
-    public int jumpheight;
+    public float horizontalspeed;
+    public int verticalspeed;
     private Vector3 move;
-    private Vector3 jump;
+    private Vector3 jump = new Vector3(0f, 2f, 0f);
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -21,20 +21,43 @@ public class PlayerMovement : MonoBehaviour
         jump = context.ReadValue<Vector2>();
     }
     // Update is called once per frame
-    void Update()
+    
+    public void Update()
     {
         movePlayer();
-        jumpPlayer();
+        //jumpPlayer();
+        verticalMovement();
+        Debug.Log(Physics.gravity);
     }
     public void movePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.z);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        transform.Translate(movement * horizontalspeed * Time.deltaTime, Space.World);
     }
     
-    public void jumpPlayer()
+    /*public void jumpPlayer()
     {
         Vector2 jumping = new Vector2(0f, jump.y);
-        transform.Translate(jumping * jumpheight * Time.deltaTime, Space.World);
+        transform.Translate(jumping * verticalspeed * Time.deltaTime, Space.World);
+    }*/
+    private void verticalMovement()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(0f, verticalspeed * Time.deltaTime, 0f);
+            Physics.gravity = new Vector3(0f, 1f, 0f);
+            //Creating a negative gravity force to create upwards movement to counteract drag.
+            //Prevents gravity from continuing to drag submarine down and create acceleration.
+        }else
+        {
+            Physics.gravity = new Vector3(0f, -1f, 0f);
+            //Changes gravity back to default once we are no longer going up.
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.position -= new Vector3(0f, verticalspeed * Time.deltaTime, 0f);
+            //Moves down.
+        }
     }
 }

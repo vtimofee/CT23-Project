bd0d3ae3a45535ab9f71908984ce24c2d99c39b9
Kevin Huang <67510498+KevinH0@users.noreply.github.com>
2023-12-15 00:05:39 -0500

    Damage mechanics update, spawns

--- City Tech Fall 2023/Assets/Script/ItemSpawns.cs
+++ City Tech Fall 2023/Assets/Script/ItemSpawns.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class ItemSpawns : MonoBehaviour
 {
     public List<GameObject> OxygenSpawner;
-    public GameObject OxygenTank;
-    public GameObject OxygenBubbles;
+    public List<GameObject> ItemLocation;
     public List<Vector3> OxygenTankVectors;
+    
     private void Awake()
     {
         oxygenSpawn();
@@ -16,8 +16,8 @@ public class ItemSpawns : MonoBehaviour
     {
         for (int i = 0; i < OxygenSpawner.Count; i++)
         {
-            Vector3 OxygenTankVectors3 = OxygenTankVectors[i];
-            Instantiate(OxygenTank, OxygenTankVectors3, Quaternion.identity);
+            Vector3 OxygenTankVectors3 = ItemLocation[i].transform.position;
+            Instantiate(OxygenSpawner[i], OxygenTankVectors3, Quaternion.identity);
         }
     }
 }
